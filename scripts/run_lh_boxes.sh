fcd88047c7c6ee879b4a709dec186087abb7541f
erik conser <econser@pdx.edu>
2018-08-18 15:47:37 -0700

    Updated leadinghorse bboxes

--- scripts/run_lh_boxes.sh
+++ scripts/run_lh_boxes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/econser/research/irsg_psu_pdx/code
 python irsg_situation.py --cfg ./configs/boxes_only.yml --b LH_FULLPOS
-#python irsg_situation.py --cfg ./configs/boxes_only.yml --b LH_HARDNEG
-#python irsg_situation.py --cfg ./configs/boxes_only.yml --b LH_FULLNEG
+python irsg_situation.py --cfg ./configs/boxes_only.yml --b LH_HARDNEG
+python irsg_situation.py --cfg ./configs/boxes_only.yml --b LH_FULLNEG
